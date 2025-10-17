d4bc79a870429149ca05b377fdec5e0e87f17e05
Soma Das <157784123+sommaas@users.noreply.github.com>
2025-10-17 11:53:56 +0600

    Updated Variable Register

--- compiler.py
+++ compiler.py
@@ -2,99 +2,114 @@ import tkinter as tk
 from tkinter import ttk, scrolledtext, messagebox
 import ply.lex as lex
 import ply.yacc as yacc
-import re
 
-# LEXICAL ANALYZER (LEX)
-class Lexer:
-    # Reserved keywords
-    reserved = {
-        'if': 'IF',
-        'else': 'ELSE',
-        'while': 'WHILE',
-        'for': 'FOR',
-        'int': 'INT',
-        'float': 'FLOAT',
-        'return': 'RETURN',
-        'print': 'PRINT',
+
+class TokenScanner:
+    keywords = {
+        'if': 'IF', 'else': 'ELSE', 'while': 'WHILE', 'for': 'FOR',
+        'int': 'INT', 'float': 'FLOAT', 'return': 'RETURN', 'print': 'PRINT'
     }
 
-    # Token list
     tokens = [
-        'ID', 'NUMBER', 'FLOAT_NUM',
-        'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MODULO',
-        'ASSIGN', 'EQ', 'NE', 'LT', 'LE', 'GT', 'GE',
-        'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE',
-        'SEMICOLON', 'COMMA',
-    ] + list(reserved.values())
+        'IDENTIFIER', 'INTEGER', 'DECIMAL',
+        'PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE', 'MOD',
+        'EQUALS', 'EQUAL_TO', 'NOT_EQUAL', 'LESS', 'LESS_EQ', 'GREATER', 'GREATER_EQ',
+        'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE', 'SEMICOLON', 'COMMA',
+    ] + list(keywords.values())
 
-    # Token rules
     t_PLUS = r'\+'
     t_MINUS = r'-'
-    t_TIMES = r'\*'
+    t_MULTIPLY = r'\*'
     t_DIVIDE = r'/'
-    t_MODULO = r'%'
-    t_ASSIGN = r'='
-    t_EQ = r'=='
-    t_NE = r'!='
-    t_LT = r'<'
-    t_LE = r'<='
-    t_GT = r'>'
-    t_GE = r'>='
+    t_MOD = r'%'
+    t_EQUALS = r'='
+    t_EQUAL_TO = r'=='
+    t_NOT_EQUAL = r'!='
+    t_LESS = r'<'
+    t_LESS_EQ = r'<='
+    t_GREATER = r'>'
+    t_GREATER_EQ = r'>='
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
     t_LBRACE = r'\{'
     t_RBRACE = r'\}'
     t_SEMICOLON = r';'
     t_COMMA = r','
-
-    # Ignored characters (spaces and tabs)
     t_ignore = ' \t'
 
-    def t_FLOAT_NUM(self, t):
+    def t_DECIMAL(self, tok):
         r'\d+\.\d+'
-        t.value = float(t.value)
-        return t
+        tok.value = float(tok.value)
+        return tok
 
-    def t_NUMBER(self, t):
+    def t_INTEGER(self, tok):
         r'\d+'
-        t.value = int(t.value)
-        return t
+        tok.value = int(tok.value)
+        return tok
 
-    def t_ID(self, t):
+    def t_IDENTIFIER(self, tok):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.reserved.get(t.value, 'ID')
-        return t
+        tok.type = self.keywords.get(tok.value, 'IDENTIFIER')
+        return tok
 
-    def t_newline(self, t):
+    def t_newline(self, tok):
         r'\n+'
-        t.lexer.lineno += len(t.value)
+        tok.lexer.lineno += len(tok.value)
 
-    def t_error(self, t):
-        self.errors.append(f"Illegal character '{t.value[0]}' at line {t.lineno}")
-        t.lexer.skip(1)
+    def t_error(self, tok):
+        self.issues.append(f"Invalid character '{tok.value[0]}' at line {tok.lineno}")
+        tok.lexer.skip(1)
 
     def __init__(self):
-        self.lexer = None
-        self.tokens_list = []
-        self.errors = []
+        self.scanner = None
+        self.token_stream = []
+        self.issues = []
 
-    def build(self):
-        self.lexer = lex.lex(module=self)
+    def initialize(self):
+        self.scanner = lex.lex(module=self)
 
-    def tokenize(self, data):
-        self.tokens_list = []
-        self.errors = []
-        self.lexer.input(data)
+    def scan(self, code):
+        self.token_stream = []
+        self.issues = []
+        self.scanner.input(code)
         
         while True:
-            tok = self.lexer.token()
+            tok = self.scanner.token()
             if not tok:
                 break
-            self.tokens_list.append({
-                'type': tok.type,
-                'value': tok.value,
-                'line': tok.lineno,
-                'position': tok.lexpos
+            self.token_stream.append({
+                'kind': tok.type,
+                'val': tok.value,
+                'ln': tok.lineno,
+                'pos': tok.lexpos
             })
         
-        return self.tokens_list, self.errors
+        return self.token_stream, self.issues
+
+
+
+# VARIABLE REGISTRY
+class VariableRegistry:
+    def __init__(self):
+        self.entries = {}
+        self.contexts = ['main']
+        
+    def add(self, identifier, var_type, initial_val=None, ctx=None):
+        ctx = ctx or self.contexts[-1]
+        key = f"{ctx}:{identifier}"
+        self.entries[key] = {
+            'id': identifier,
+            'dtype': var_type,
+            'val': initial_val,
+            'ctx': ctx
+        }
+    
+    def find(self, identifier):
+        for ctx in reversed(self.contexts):
+            key = f"{ctx}:{identifier}"
+            if key in self.entries:
+                return self.entries[key]
+        return None
+    
+    def all_entries(self):
+        return list(self.entries.values())
